75a54bc37178c35497553c2bb301dd615129b07d
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-08 22:20:52 +0500

    Add files via upload

---
+++ Guest_list/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Imran Khan', 'Mawaz', ' zardari'];
+for (let i = 0; i < Guest_list.length; i) {
+    console.log('Dear MR.' + Guest_list[0] + '\n\n it is our pleasure to invite you in party. \n Thank you');
+}
---
+++ Guest_list/main.ts
@@ -0,0 +1,8 @@
+
+let Guest_list : string [] = ['Imran Khan' , 'Mawaz' ,' zardari'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+console.log( 'Dear MR.' + Guest_list[i ] + '\n\n it is our pleasure to invite you in party. \n Thank you')
+
+}
