7a2cb7d1a0bd2533612b0abdf27759066ce00a46
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-08 22:14:09 +0500

    Add files via upload

---
+++ Greetings/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Asaad", "Anas", "Aqdas"];
+const message = "welcome to typescript class  ";
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Greetings/main.ts
@@ -0,0 +1,8 @@
+let names :string[] = ["Asaad", "Anas","Aqdas"];
+const message : string = "welcome to typescript class  "
+for (let i=0; i<names.length; i++) {
+
+
+    console.log(message + names[i]);
+}
+
