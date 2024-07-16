2615a31ac1d1c002340e5e160ddf0f0575987da9
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 21:05:24 +0500

    Add files via upload

---
+++ Ordinal_endings/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let num of numbers) {
+    let ordinal_endings;
+    if (num === 1) {
+        ordinal_endings = 'st';
+    }
+    else if (num === 2) {
+        ordinal_endings = 'nd';
+    }
+    else if (num === 3) {
+        ordinal_endings = 'rd';
+    }
+    else
+        (ordinal_endings = 'th');
+    console.log(`${num}${ordinal_endings}`);
+}
