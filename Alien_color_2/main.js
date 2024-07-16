19a4d05a536b5ae97629ab78850fc540f78e94d0
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:54:42 +0500

    Add files via upload

---
+++ Alien_color_2/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let alien_color = "Green";
+if (alien_color === "Green") {
+    console.log("you got 5 points");
+}
+else {
+    console.log("You got 1 points");
+}
+;
+alien_color = "yellow";
+if (alien_color === "Green") {
+    console.log("you got 5 points");
+}
+else {
+    console.log("You got 1 points");
+}
+;
---
+++ Alien_color_2/main.ts
@@ -0,0 +1,20 @@
+let alien_color : string = "Green";
+
+if (alien_color === "Green") {
+
+    console.log("you got 5 points")
+}
+else{
+
+    console.log("You got 10 points")
+};
+
+alien_color = "yellow";
+if (alien_color === "Green") {
+
+    console.log("you got 5 points")
+}
+else{
+
+    console.log("You got 10 points")
+};
