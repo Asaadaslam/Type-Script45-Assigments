7c86938e5085f1d9f11b1fd4e057e0879ceb32c5
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:53:06 +0500

    Add files via upload

---
+++ Alien_color_1/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just got 5 points");
+}
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just got 5 points");
+}
