774cbbb5f6bb858831bc25ef2862fa842780434c
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:56:55 +0500

    Add files via upload

---
+++ Alien_color_3/main.js
@@ -0,0 +1,57 @@
+"use strict";
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("player earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earn 10 points");
+}
+else if (alien_color === "red") {
+    console.log("player earn 15 points");
+}
+else {
+    console.log("please choose color");
+}
+;
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earn 10 points");
+}
+else if (alien_color === "red") {
+    console.log("player earn 15 points");
+}
+else {
+    console.log("please choose color");
+}
+;
+alien_color = "red";
+if (alien_color === "green") {
+    console.log("player earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earn 10 points");
+}
+else if (alien_color === "red") {
+    console.log("player earn 15 points");
+}
+else {
+    console.log("please choose color");
+}
+;
+alien_color = "";
+if (alien_color === "green") {
+    console.log("player earn 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earn 10 points");
+}
+else if (alien_color === "red") {
+    console.log("player earn 15 points");
+}
+else {
+    console.log("please choose color");
+}
+;
---
+++ Alien_color_3/main.ts
@@ -0,0 +1,74 @@
+let alien_color :string = "green";
+
+
+if (alien_color === "green") {
+
+    console.log("player earn 5 points")
+}
+else if (alien_color === "yellow") {
+
+    console.log("player earn 10 points")   
+}
+else if (alien_color === "red") {
+
+    console.log("player earn 15 points")   
+}
+else {
+    console.log("please choose color")
+};
+
+
+
+alien_color ="yellow";
+
+if (alien_color === "green") {
+
+    console.log("player earn 5 points")
+}
+else if (alien_color === "yellow") {
+
+    console.log("player earn 10 points")   
+}
+else if (alien_color === "red") {
+
+    console.log("player earn 15 points")   
+}
+else {
+    console.log("please choose color")
+};
+
+
+alien_color = "red";
+
+if (alien_color === "green") {
+
+    console.log("player earn 5 points")
+}
+else if (alien_color === "yellow") {
+
+    console.log("player earn 10 points")   
+}
+else if (alien_color === "red") {
+
+    console.log("player earn 15 points")   
+}
+else {
+    console.log("please choose color")
+};
+
+alien_color = "";
+if (alien_color === "green") {
+
+    console.log("player earn 5 points")
+}
+else if (alien_color === "yellow") {
+
+    console.log("player earn 10 points")   
+}
+else if (alien_color === "red") {
+
+    console.log("player earn 15 points")   
+}
+else {
+    console.log("please choose color")
+};
