1fcf408f2332c2a2cbf1d073b7a0a4a3ae62b85f
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 21:00:28 +0500

    Add files via upload

---
+++ Favourite_fruits/main.js
@@ -0,0 +1,19 @@
+"use strict";
+let favourite_fruits = ['banana', 'orange', 'apple'];
+if (favourite_fruits.includes('banana')) {
+    console.log(" i really like banana");
+}
+if (favourite_fruits.includes('orange')) {
+    console.log(" i really like orange");
+}
+if (favourite_fruits.includes('apple')) {
+    console.log(" i really like apple");
+}
+if (favourite_fruits.includes('Grapes')) {
+    console.log(" i really like Grapes");
+}
+if (favourite_fruits.includes('Pear')) {
+    console.log(" i really like Pear");
+}
+else
+    ("i dont't like fruits");
---
+++ Favourite_fruits/main.ts
@@ -0,0 +1,26 @@
+let favourite_fruits : string[] = ['banana', 'orange', 'apple'];
+
+if (favourite_fruits.includes('banana')){
+
+    console.log(" i really like banana")
+}
+
+if (favourite_fruits.includes('orange')){
+
+    console.log(" i really like orange")
+}
+
+if (favourite_fruits.includes('apple')){
+
+    console.log(" i really like apple")
+}
+
+if (favourite_fruits.includes('Grapes')){
+
+    console.log(" i really like Grapes")
+}
+
+if (favourite_fruits.includes('Pear')){
+
+    console.log(" i really like Pear")
+}
