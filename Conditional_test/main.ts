839fe20b8dd7a8770f94f068eea108c6f23efef4
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:50:19 +0500

    Add files via upload

---
+++ Conditional_test/main.ts
@@ -0,0 +1,32 @@
+let car : string = "subaru"
+
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+
+
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+console.log("Is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+
+console.log("Is car.upper case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+console.log("Is car.lower case == 'SUBARU'? I predict False.");
+console.log(car.toLowerCase() == 'SUBARU');
+
+console.log("Is car == 'SUBARU'? I predict False.");
+console.log(car  == 'SUBARU');
+
+console.log("Is car == 'Train'? I predict False.");
+console.log(car  == 'Train');
+
+console.log("Is car == 'Bus'? I predict False.");
+console.log(car  == 'Bus');
+
+console.log("Is car == 'cycle'? I predict False.");
+console.log(car  == 'cycle');
