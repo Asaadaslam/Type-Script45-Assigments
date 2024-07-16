bcad3ebb832391231e21f592e9c5c204bb8470c5
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 21:07:53 +0500

    Add files via upload

---
+++ Animals/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let animals = ['Dog', 'Cat', 'Horse'];
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet`);
+}
---
+++ Animals/main.ts
@@ -0,0 +1,7 @@
+let animals : string [] = ['Dog' , 'Cat' , 'Horse'];
+
+for(let animal of animals){
+
+console.log(`A ${animal} would make a great pet`)
+
+}
