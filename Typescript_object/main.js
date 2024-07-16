eee769d0c7dc9fcf8d6b0c475868eeee4e6a464c
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:43:38 +0500

    Add files via upload

---
+++ Typescript_object/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let car = {
+    model: 2000,
+    make: ' Toyota',
+    color: 'grey',
+    automatic: true
+};
+console.log(car);
---
+++ Typescript_object/main.ts
@@ -0,0 +1,18 @@
+interface car {
+    model : number,
+    make : string,
+    color : string,
+    automatic : boolean,
+
+
+}
+
+let car = {
+    model :2000,
+    make : ' Toyota',
+    color : 'grey',
+    automatic : true
+
+}
+
+console.log(car)
