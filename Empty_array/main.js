bdd7c53bfa943873fa555ef816241975e27a6959
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:40:18 +0500

    Add files via upload

---
+++ Empty_array/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let Item = ['Pakistan', 'India', 'America'];
+let item2 = [];
+item2.push('Germany');
+item2.push('Brazil');
+item2.push('Italy');
+console.log(item2);
+console.log(Item);
---
+++ Empty_array/main.ts
@@ -0,0 +1,11 @@
+let Item : string[] = ['Pakistan', 'India', 'America'];
+
+let item2 : string[] = [];
+
+item2.push('Germany');
+item2.push('Brazil');
+item2.push('Italy');
+
+console.log(item2);
+
+console.log(Item);
