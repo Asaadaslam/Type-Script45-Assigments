623e276112c4d37dc0425864c75027eaede9bf18
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-16 20:32:58 +0500

    Add files via upload

---
+++ seeing_the_world/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let places = ['italy', 'germany', 'Agra', 'Eifel towers', 'time square'];
+console.log('orignal ' + places);
+console.log('copy ' + [...places].sort());
+console.log('orignal' + places);
+console.log('copy ' + [...places].sort().reverse());
+console.log(places.reverse());
+console.log(places.reverse());
+console.log(places.sort());
+console.log(places.sort().reverse());
---
+++ seeing_the_world/main.ts
@@ -0,0 +1,18 @@
+let places : string[] = ['italy', 'germany' , 'Agra', 'Eifel towers', 'time square'];
+
+console.log('orignal ' + places);
+
+console.log('copy ' + [...places].sort());
+
+console.log('orignal' + places);
+
+console.log('copy ' + [...places].sort().reverse());
+
+console.log(places.reverse());
+
+console.log(places.reverse());
+
+console.log(places.sort());
+
+console.log(places.sort().reverse());
+
