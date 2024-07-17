7a13717b6e95d94f0f7b0a509c04c9ce1994e696
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-17 10:13:47 +0500

    Add files via upload

---
+++ return_cities/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+console.log(city_country('karachi', 'pakistan'));
+console.log(city_country('Sydney', 'Austrailia'));
+console.log(city_country('Tokyo', 'Japan'));
