05583a88c34420040536fe12632bca247c25485b
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-17 10:11:23 +0500

    Add files via upload

---
+++ Large_shirt/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size = "large", text = "I love Type script") {
+    console.log(`creating a ${size} shirt with ${text} message on it`);
+}
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "I love python");
---
+++ Large_shirt/main.ts
@@ -0,0 +1,10 @@
+function make_shirt (size : string = "large" , text : string = "I love Type script") {
+
+    console.log(`creating a ${size} shirt with ${text} message on it`)
+}
+
+make_shirt()
+
+make_shirt("medium")
+
+make_shirt("small" , "I love python");
