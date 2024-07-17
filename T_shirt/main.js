baadbba7228e92f89ba55bc6e0f3917b62f891c6
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-17 10:06:03 +0500

    Add files via upload

---
+++ T_shirt/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, text) {
+    console.log(`creating a ${size} with message ${text}`);
+}
+make_shirt('large', 'Hello');
+make_shirt('madium', 'Hello');
+make_shirt('small', 'Hello');
---
+++ T_shirt/main.ts
@@ -0,0 +1,9 @@
+function make_shirt(size:string , text:string)
+{
+    console.log(`creating a ${size} with message ${text}`);
+
+}
+
+make_shirt('large' , 'Hello');
+make_shirt('madium' , 'Hello');
+make_shirt('small' , 'Hello');
