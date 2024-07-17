5e6c25e92b7bc6e9f66cb7f6ed53dd02e6375c91
Muhammad Asaad Aslam <muhammadasaad30@gmail.com>
2024-07-17 10:16:00 +0500

    Add files via upload

---
+++ magician/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ['Harry Potter', 'Hermone', 'Rod', 'Dumble dor'];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magicians);
